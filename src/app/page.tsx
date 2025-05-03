e271aa7f8b2056fcab73eb2a4b9a243936a68351
geekyntation <geekyntation@gmail.com>
2025-05-03 18:23:04 +0700

    fix: disable scroll on splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Intro from "@/components/layout/intro";
 import SplashScreen from "@/components/layout/splashScreen";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isSplash, setIsSplash] = useState<boolean>(true)
@@ -12,8 +12,14 @@ export default function Home() {
     setIsFading(true)
     setTimeout(() => {
       setIsSplash(false)
+      document.body.classList.remove('overflow-hidden')
     }, 1000)
   }
+
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden')
+  }, [])
+
   return (
     <main className="w-full max-w-md mx-auto overflow-hidden">
       {isSplash && (
