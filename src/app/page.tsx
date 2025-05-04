f0a8e2a5a973199821084e61da32a7e7f9710a50
geekyntation <geekyntation@gmail.com>
2025-05-04 16:04:49 +0700

    fix: fix splashscreen issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import Comments from "@/components/layout/comments";
 import DateAndPlace from "@/components/layout/dateAndPlace";
 import Gallery from "@/components/layout/gallery";
 import Intro from "@/components/layout/intro";
-import SplashScreen from "@/components/layout/splashScreen";
-import { useEffect, useRef, useState } from "react";
+import { Loader2Icon } from "lucide-react";
+import { lazy, Suspense, useEffect, useRef, useState } from "react";
+
+const SplashScreen = lazy(() => import('@/components/layout/splashScreen'))
 
 export default function Home() {
   const [isSplash, setIsSplash] = useState<boolean>(true)
@@ -32,7 +34,13 @@ export default function Home() {
   return (
     <main className="w-full max-w-md mx-auto overflow-hidden">
       {isSplash && (
-        <SplashScreen isFading={isFading} onClose={onSplashClose} />
+        <Suspense fallback={
+          <div className="w-full h-svh grid place-items-center bg-red-950 brightness-75">
+            <Loader2Icon className="w-8 h-8 animate-spin" />
+          </div>
+        }>
+          <SplashScreen isFading={isFading} onClose={onSplashClose} />
+        </Suspense>
       )}
 
       <audio loop ref={audioRef} src="/song.mp3" />
