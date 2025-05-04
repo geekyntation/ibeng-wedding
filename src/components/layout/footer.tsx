4f5e6a7262949e80107a0d5d7aea9cfc6a458213
geekyntation <geekyntation@gmail.com>
2025-05-04 18:56:39 +0700

    feat(footer): add ending message

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,12 +1,22 @@
 import { ExternalLinkIcon } from "lucide-react"
-import { Raleway } from "next/font/google"
+import { Dancing_Script, Raleway } from "next/font/google"
 import { twMerge } from "tailwind-merge"
 
 const raleway = Raleway({ subsets: ['latin'] })
+const ds = Dancing_Script({ subsets: ['latin'] })
 
 export default function Footer() {
     return (
-        <section id="footer" className={twMerge(raleway.className, "w-full px-6 py-2 text-center text-white text-xs")}>
+        <section id="footer" className={twMerge(raleway.className, "w-full flex flex-col gap-8 px-6 py-2 text-center text-white text-xs")}>
+            <div className="w-full flex flex-col gap-2">
+                <p className="text-center">{"Atas kehadiran dan do'a restu Bapak/Ibu/Saudara/i sekalian, kami mengucapkan banyak Terima Kasih."}</p>
+
+                <div className="flex flex-col gap-2">
+                    <p>{"Kami yang Berbahagia"}</p>
+
+                    <p className={twMerge(ds.className, 'text-4xl')}>Renaldi & Wulan</p>
+                </div>
+            </div>
             <span>
                 <span>Built with 🤍 by </span>
                 <a href="https://wa.link/2p96tr" target="_blank" className="inline-flex items-center gap-1 underline">
